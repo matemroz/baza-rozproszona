c2bef4ba884ad28d0c780d72ae9ee885d5953e5a
matemroz@gmail.com <matemroz@gmail.com@5ecf1467-e387-112c-24b8-8481ffc6bd83>
2011-11-24 00:01:57 +0000

    Szkielety - Walidatory

---
+++ BankDomain/src/pw/bdwsr/rozproszonaprojekt/db/validation/KlientValidator.java
@@ -0,0 +1,40 @@
+package pw.bdwsr.rozproszonaprojekt.db.validation;
+
+public class KlientValidator {
+	public static boolean validatePesel(String pesel){
+		return true;
+	}
+	
+	public static boolean validateImie(String imie){
+		return true;
+	}
+	
+	public static boolean validateNazwisko(String nazwisko){
+		return true;
+	}
+	
+	public static boolean validateUlicaZamieszkania(String ulicaZamieszkania){
+		return true;
+	}
+	
+	public static boolean validateNumerDomu(String numerDomu){
+		return true;
+	}
+	
+	public static boolean validateNumerMieszkania(String numerMieszkania){
+		return true;
+	}
+
+	public static boolean validateNumerTelefonu(String numerTelefonu){
+		return true;
+	}
+	
+	public static boolean validateNumerDowoduOsobistego(String numerDowoduOsobistego){
+		return true;
+	}
+	
+	public static boolean validateNumerPaszportu(String numerPaszportu){
+		return true;
+	}
+	
+}
---
+++ BankDomain/src/pw/bdwsr/rozproszonaprojekt/db/validation/KontoValidator.java
@@ -0,0 +1,19 @@
+package pw.bdwsr.rozproszonaprojekt.db.validation;
+
+import pw.bdwsr.rozproszonaprojekt.domain.RodzajKonta;
+
+public class KontoValidator {
+
+	public static boolean validateNrKonta(String nrKonta){
+		return true;
+	}
+	
+	public static boolean validateSrodki(double srodki){
+		return true;
+	}
+	
+	public static boolean validateRodzajKonta(RodzajKonta rodzajKonta){
+		return true;
+	}
+	
+}
