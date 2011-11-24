a85ddc6a20e5ac40e85712ebcebe0284e7e071cb
bieniek.greg <bieniek.greg@gmail.com>
2011-11-24 14:29:58 +0000

    -dodano konstruktor do Klient -w MongoKlientDAO dodano metode pobierania klientow wedlug kryterium (wypelnia się klase Klient) -zmodyfikowano interfejs KlientDAO

--- BankDomain/src/pw/bdwsr/rozproszonaprojekt/db/dao/KlientDAO.java
+++ BankDomain/src/pw/bdwsr/rozproszonaprojekt/db/dao/KlientDAO.java
@@ -6,7 +6,7 @@ import pw.bdwsr.rozproszonaprojekt.domain.Klient;
 
 public interface KlientDAO {
 	public Klient pobierzKlienta(String pesel);
-	public List<Klient> pobierzKlientowNaPodstawieKryterium(String kryterium);
+	public List<Klient> pobierzKlientowNaPodstawieKryterium(Klient klientKryterium);//wypelnione tylko pola, ktore maja stanowic kryterium wyszukiwania
 	public boolean zmienDaneKlienta(Klient klient);
 	public boolean usunKlienta(String pesel);
 	public boolean dodajKlienta(Klient klient);
